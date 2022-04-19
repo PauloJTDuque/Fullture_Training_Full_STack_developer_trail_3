30673af75ceb55f815c0742873a744489f884a6d
Paulo Duque <paulo.duque@terra.com.br>
2022-04-19 08:06:15 -0300

    Alterando o Exercicio 5

--- Exercicio f5/js/script.js
+++ Exercicio f5/js/script.js
@@ -1,25 +1,31 @@
-// v_pos_number=0
-// v_neg_number=0
+v_pollution_index=0
+v_continue='s'
 
-// v_ent_user=prompt("Digite um número diferente de 0:");
-// while(v_ent_user == 0){
-//     v_ent_user=prompt("O número deve ser diferente de 0:");
-// }
+while (v_continue=="s"){
 
-// if (v_ent_user > 0){
-//     v_pos_number = v_ent_user;
-//     alert("Numero Positivo:"+v_pos_number);
-// }
-// else{
-//     v_neg_number = v_ent_user;
-//     alert("Numero Negativo:"+v_neg_number);
-// }  
+	// Lendo o Índice de Poluição
+	v_pollution_index = Number(prompt("Digite o Índice de Poluição da Empresa: " + v_name_company));
 
-var n = prompt("Digite um numero");
-var total=n/2;
-if(n%2 == 0){
-	alert("Par");
-}else{
-	alert("Impar");
+	while (v_pollution_index < 0 || v_pollution_index > 10){
+		v_pollution_index = Number(prompt("O Índice de Poluição deve estar entre 0 e 1: "));
+	}
+	// Classificando a Empresa e emitindo parecer
+	if (v_pollution_index >= 0.3 && v_pollution_index < 0.4){
+		alert( " Intimamos as Empresas do 1º Grupo a SUSPENDEREM suas atividades")
+	} else if (v_pollution_index >= 0.4 && v_pollution_index < 0.5) {
+		alert( " Intimamos as Empresas do 1º e 2º Grupo a SUSPENDEREM suas atividades")
+	} else if (v_pollution_index >= 0.4 && v_pollution_index < 0.5) {
+		alert( " Notificamos as Empresas de todos os Grupos a PARALISAREM suas atividades")
+	} else {
+		alert( " Empresas estão dentro dos parâmetros de poluição estabelecidos. Bom trabalho!")
+	}
+	// Verificando se devemos continuar
+	v_continue = prompt("Digite 's' para continuar ou 'n' para encerrar: ");
+	while (v_continue !== 's' && v_continue !== 'n'){
+		v_continue = prompt("Favor informar 's' ou 'n': ");
+	}
+	if (v_continue == 'n'){
+		alert( " Rotina Encerrada pelo Usuário ")
+		break
+	}
 }
-alert(total);
